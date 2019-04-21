7f3f2076257077223cba1e0b5af9b6a67dbdd1ba
Bashi Samarakoon <himansa.samarakoon@gmail.com>
2019-04-21 18:03:32 +0530

    First commit

---
+++ panel.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GOTAXI | Admin Panel</title>
+	<link rel="stylesheet" type="text/css" href="panelstyles.css">
+    <link rel="shortcut icon" type="image/png" href="img/favicon2.png">
+	
+</head>
+<body>
+	<header class="header">
+		<div class="header__text-box">
+                <h1 class="heading-primary">
+                <span class="heading-primary--main"><span class="go">GO</span>TAXI</span>
+                <span class="heading-primary--sub">Your traveling companion</span>
+                </h1>
+                       
+                    <a href="driverdetails.php" class="btn btn--white btn--animated">Driver Details</a>
+                    <a href="vehicledetails.php" class="btn btn--white btn--animated">Vehicle Details</a>                         
+            </div>
+	</header>
+</body>
+</html>
